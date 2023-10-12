90f6afb7884d75fa67af30cdad9f4f5ced163181
nana-Kwon <chanyeol0615@naver.com>
2023-10-12 21:24:38 +0900

    5

--- script/script.js
+++ script/script.js
@@ -1,11 +1,17 @@
+// script.js
+
 let wordList = [];
+
+function validateInput(input) {
+    return input.trim() !== '';
+}
 
 function validateForm() {
     let level = document.getElementById("level").value;
     let word = document.getElementById("word").value;
     let meaning = document.getElementById("meaning").value;
 
-    if (!level || !word || !meaning) {
+    if (!validateInput(level) || !validateInput(word) || !validateInput(meaning)) {
         alert("모든 필드를 채워주세요.");
         return false;
     }
@@ -17,8 +23,54 @@ function validateForm() {
     return true;
 }
 
-// Rest of the functions (editWord, deleteWord, displayAllWords) remain unchanged.
+function editWord() {
+    let keyword = prompt("수정할 단어를 입력하세요:");
+
+    // Validate the keyword
+    if (!validateInput(keyword)) {
+        console.log("유효하지 않은 키워드입니다.");
+        return;
+    }
+
+    let matchingWords = [];
+
+    // Find words that match the keyword
+    for (let i = 0; i < wordList.length; i++) {
+        if (wordList[i].word.includes(keyword)) {
+            matchingWords.push({ index: i, word: wordList[i] });
+        }
+    }
+
+    if (matchingWords.length === 0) {
+        console.log("일치하는 단어를 찾을 수 없습니다.");
+        return;
+    }
+
+    console.log("일치하는 단어를 선택하세요:");
+    for (let i = 0; i < matchingWords.length; i++) {
+        console.log(i + 1, matchingWords[i].word.word);
+    }
+
+    let selectedWordIndex = prompt("선택할 번호를 입력하세요:") - 1;
+    if (selectedWordIndex < 0 || selectedWordIndex >= matchingWords.length) {
+        console.log("올바르지 않은 번호입니다.");
+        return;
+    }
+
+    let newMeaning = prompt("새로운 뜻을 입력하세요:");
+
+    // Validate the new meaning
+    if (!validateInput(newMeaning)) {
+        console.log("유효하지 않은 뜻입니다.");
+        return;
+    }
+
+    wordList[matchingWords[selectedWordIndex].index].meaning = newMeaning;
+
+    console.log("단어가 수정되었습니다.");
+}
 
 // 예시로 단어 목록을 초기화합니다.
 wordList.push({ level: 1, word: "Apple", meaning: "A fruit" });
 wordList.push({ level: 2, word: "Banana", meaning: "A yellow fruit" });
+
