2018985765389c9beb8885438f294758e4d83ed5
nana-Kwon <chanyeol0615@naver.com>
2023-10-12 22:16:47 +0900

    5

--- script/script.js
+++ script/script.js
@@ -20,37 +20,23 @@ function validateForm() {
   wordList.push(newWord);
   alert("새 단어가 단어장에 추가되었습니다.");
 
+  displayAllWords();  // 단어 추가 후 목록 표시
+
   return true;
 }
 
-// 수정된 editWord 함수. editFunction.js 파일과 중복 정의를 피하기 위해 이름을 변경했습니다.
-function editSelectedWord() {
-  let keyword = prompt("수정할 단어를 입력하세요:");
-  // 나머지 코드는 기존과 동일하게 유지합니다.
-}
-
-function deleteWord() {
-  let keyword = prompt("삭제할 단어를 입력하세요:");
-
-  for (let i = 0; i < wordList.length; i++) {
-    if (wordList[i].word.includes(keyword)) {
-      wordList.splice(i, 1);
-      console.log("단어가 삭제되었습니다.");
-      return;
-    }
-  }
-
-  console.log("일치하는 단어를 찾을 수 없습니다.");
-}
-
 function displayAllWords() {
-  console.log("전체 단어 목록:");
+  let wordListDiv = document.getElementById('wordList');
+  wordListDiv.innerHTML = '';  // 기존 내용 비우기
+
+  // 단어 목록 표시
   for (let i = 0; i < wordList.length; i++) {
-    console.log(i + 1, "-", wordList[i].level, wordList[i].word, wordList[i].meaning);
+    let wordInfo = document.createElement('p');
+    wordInfo.textContent = `${i + 1}. Level ${wordList[i].level}: ${wordList[i].word} - ${wordList[i].meaning}`;
+    wordListDiv.appendChild(wordInfo);
   }
 }
 
 // 예시로 단어 목록을 초기화합니다.
 wordList.push({ level: 1, word: "사과", meaning: "과일" });
 wordList.push({ level: 2, word: "바나나", meaning: "노란 과일" });
-
