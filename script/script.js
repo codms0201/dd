9312794fdf3d9ead1c339198ba5ff5d04c71d35b
nana-Kwon <chanyeol0615@naver.com>
2023-10-12 22:34:17 +0900

    ㄴㅇ

--- script/script.js
+++ script/script.js
@@ -1,42 +1,39 @@
-// script.js
-
-let wordList = [];
+let wordList = []; // 단어 목록
 
 function validateInput(input) {
-  return input.trim() !== '';
+    return input.trim() !== '';
 }
 
-function validateForm() {
-  let level = document.getElementById("level").value;
-  let word = document.getElementById("word").value;
-  let meaning = document.getElementById("meaning").value;
+function validateForm(event) {
+    event.preventDefault(); // 폼 제출 기본 동작 중지
 
-  if (!validateInput(level) || !validateInput(word) || !validateInput(meaning)) {
-    alert("모든 필드를 채워주세요.");
-    return false;
-  }
+    let level = document.getElementById("level").value;
+    let word = document.getElementById("word").value;
+    let meaning = document.getElementById("meaning").value;
 
-  let newWord = { level: level, word: word, meaning: meaning };
-  wordList.push(newWord);
-  alert("새 단어가 단어장에 추가되었습니다.");
+    if (!validateInput(level) || !validateInput(word) || !validateInput(meaning)) {
+        alert("모든 필드를 채워주세요.");
+        return false;
+    }
 
-  displayAllWords();  // 단어 추가 후 목록 표시
+    // 새 단어 추가
+    let newWord = { level: level, word: word, meaning: meaning };
+    wordList.push(newWord);
 
-  return true;
+    // 단어 목록 표시
+    displayAllWords();
+
+    alert("새 단어가 단어장에 추가되었습니다.");
+    return true;
 }
 
 function displayAllWords() {
-  let wordListDiv = document.getElementById('wordList');
-  wordListDiv.innerHTML = '';  // 기존 내용 비우기
+    let wordListDiv = document.getElementById("wordList");
+    wordListDiv.innerHTML = ''; // 기존 내용 초기화
 
-  // 단어 목록 표시
-  for (let i = 0; i < wordList.length; i++) {
-    let wordInfo = document.createElement('p');
-    wordInfo.textContent = `${i + 1}. Level ${wordList[i].level}: ${wordList[i].word} - ${wordList[i].meaning}`;
-    wordListDiv.appendChild(wordInfo);
-  }
+    for (let i = 0; i < wordList.length; i++) {
+        let wordItem = document.createElement('div');
+        wordItem.textContent = `${i + 1}. ${wordList[i].level} - ${wordList[i].word} (${wordList[i].meaning})`;
+        wordListDiv.appendChild(wordItem);
+    }
 }
-
-// 예시로 단어 목록을 초기화합니다.
-wordList.push({ level: 1, word: "사과", meaning: "과일" });
-wordList.push({ level: 2, word: "바나나", meaning: "노란 과일" });
